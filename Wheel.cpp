63e4685659aac817c876074ec18c53a44bf44b01
Hugo <hugo3.weibel@gmail.com>
2025-03-09 11:22:39 +0100

    Begining of Wheel clas development

--- Wheel.cpp
+++ Wheel.cpp
@@ -1,8 +1,81 @@
 #include "Wheel.h"
 
+//----- CONSTRUCTOR -----//
 Wheel::Wheel() {
 
+	z = 0;
+	d = 0.f;
+	m = 0.f;
 }
 
-Wheel::~Wheel() {
+Wheel::Wheel(int n_z, float n_m){
+
+	z = n_z;
+	m = n_m;
+
+	d = z * m;
+}
+
+Wheel::Wheel(int n_d, float n_m){
+
+	d = n_d;
+	m = n_m;
+
+	if (m != 0) {
+		z = d / m;
+	}
+	else {
+		std::cout << "========================================" << std::endl;
+		std::cout << "*** Wheel(int n_d, float n_m)***" << std::endl;
+		std::cout << "Z COMPUTATION IMPOSSIBLE (M = 0)" << std::endl;
+		std::cout << "========================================" << std::endl;
+	}
+}
+
+//----- DESTRUCTOR -----//
+Wheel::~Wheel() {}
+
+
+//----- GETER -----//
+int Wheel::	get_z(){
+
+	return(z);
+}
+
+float Wheel::get_d(){
+
+	return(d);
+}
+
+float Wheel::get_m(){
+	
+	return(m);
+}
+
+//----- SETER -----//
+void Wheel::set_z(int n_z){
+
+	z = n_z;
+	return;
+}
+
+void Wheel::set_d(float n_d){
+
+	d = n_d;
+	return;
+}
+
+void Wheel::set_m(float n_m){
+
+	m = n_m;
+	return;
+}
+
+
+//----- METHODS -----//
+void Wheel::print_wheel() {
+
+	std::cout << "z = " << z << std::endl;
+	std::cout << "d = " << d << "_mm" << std::endl;
+	std::cout << "m = " << m << "_mm" << std::endl;
 }
--- Wheel.h
+++ Wheel.h
@@ -1,11 +1,43 @@
 #pragma once
 
+#include <iostream>
+
 class Wheel{
 
+private:
+
+	/*
+		z : Wheel's number of theeth
+		d : Wheel's diameter (mm)
+		m : Wheel's module (mm)
+	*/
+
+	int z;
+
+	float d;
+	float m;
+
 public:
+
+	//----- CONSTRUCTOR -----//
 	Wheel();
+
+	Wheel(int n_z, float n_m);
+
+	//----- DESTRUCTOR -----//
 	~Wheel();
 
-private:
+	//----- GETER -----//
+	int		get_z();
+	float	get_d();
+	float	get_m();
+
+	//----- SETER -----//
+	void set_z(int n_z);
+	void set_d(float n_d);
+	void set_m(float n_m);
+
+	//----- METHODS -----//
+	void print_wheel();
 
 };
